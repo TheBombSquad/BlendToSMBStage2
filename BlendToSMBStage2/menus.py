683ad1e4ddabf9c21e33e77d8fde9f4bb70d081a
alion8me <nrk0208@gmail.com>
2022-05-30 20:08:01 -0500

    smb checker
    smb checker project mode (smart resolution scaling)

--- BlendToSMBStage2/menus.py
+++ BlendToSMBStage2/menus.py
@@ -32,10 +32,48 @@ class VIEW3D_OT_cube_project_fixed(bpy.types.Operator):
         bpy.context.scene.tool_settings.transform_pivot_point = old_pivot_point
 
         return {"FINISHED"}
+        
+class VIEW3D_OT_cube_project_smb_checker(bpy.types.Operator):
+    bl_idname = "uv.cube_project_smb_checker"
+    bl_label = "SMB Checker Cube Projection [b2smb]"
+
+    objects: bpy.props.StringProperty()
+
+    def execute(self, context):
+        old_cursor_loc = bpy.context.scene.cursor.location.copy()
+        old_cursor_rot_euler = bpy.context.scene.cursor.rotation_euler.copy()
+        old_cursor_rot_quat = bpy.context.scene.cursor.rotation_quaternion.copy()
+        old_cursor_rot_mode = bpy.context.scene.cursor.rotation_mode
+        old_pivot_point = bpy.context.scene.tool_settings.transform_pivot_point
+
+        bpy.context.scene.cursor.location = (0.0, 0.0, 0.0)
+        bpy.context.scene.cursor.rotation_mode = "XYZ"
+        bpy.context.scene.cursor.rotation_euler = (0.0, 0.0, 0.0)
+        bpy.context.scene.tool_settings.transform_pivot_point = "CURSOR"
+        
+        #find scale factor
+        scale_factor = bpy.context.active_object.active_material.node_tree.nodes.get("Image Texture").image.size[0]
+        print(scale_factor)
+        
+        bpy.ops.uv.cube_project(
+            cube_size=1.0*256.0/scale_factor,
+            correct_aspect=True,
+            clip_to_bounds=False,
+            scale_to_bounds=False
+        )
+
+        bpy.context.scene.cursor.location = old_cursor_loc
+        bpy.context.scene.cursor.rotation_mode = old_cursor_rot_mode
+        bpy.context.scene.cursor.rotation_euler = old_cursor_rot_euler
+        bpy.context.scene.cursor.rotation_quaternion = old_cursor_rot_quat
+        bpy.context.scene.tool_settings.transform_pivot_point = old_pivot_point
+
+        return {"FINISHED"}
 
 
 def menu_func(self, context):
     self.layout.operator(VIEW3D_OT_cube_project_fixed.bl_idname)
+    self.layout.operator(VIEW3D_OT_cube_project_smb_checker.bl_idname)
 
 
 def handle_register():
