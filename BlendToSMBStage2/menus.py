5f45b642df04dab6f926fa43481a3893f204e456
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2022-08-19 03:09:47 -0400

    UV tool name clarification, minor code cleanup

--- BlendToSMBStage2/menus.py
+++ BlendToSMBStage2/menus.py
@@ -5,10 +5,10 @@ import mathutils
 
 class VIEW3D_OT_cube_project_smb_checker(bpy.types.Operator):
     bl_idname = "uv.cube_project_smb_checker"
-    bl_label = "SMB Checker Cube Projection [b2smb]"
+    bl_label = "Cube Projection (Checkers) [b2smb]"
     
     bl_options = {'REGISTER', 'UNDO'}
-    checker_size: bpy.props.IntProperty(name = "Checker Size", default=64)
+    checker_size: bpy.props.IntProperty(name = "2x2 Checker Size", default=128, min=1)
 
     def execute(self, context):
         old_cursor_loc = bpy.context.scene.cursor.location.copy()
@@ -22,7 +22,7 @@ class VIEW3D_OT_cube_project_smb_checker(bpy.types.Operator):
         bpy.context.scene.cursor.rotation_euler = (0.0, 0.0, 0.0)
         bpy.context.scene.tool_settings.transform_pivot_point = "CURSOR"
         
-        #find scale factor
+        # Find scale factor
         scale_factor = bpy.context.active_object.active_material.node_tree.nodes.get("Image Texture").image.size[0]
         
         bpy.ops.uv.cube_project(
@@ -42,20 +42,19 @@ class VIEW3D_OT_cube_project_smb_checker(bpy.types.Operator):
 
 class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
     bl_idname = "uv.checker_faq"
-    bl_label = "SMB Checker FAQ [b2smb]"
+    bl_label = "Smart Follow Active Quads (Checkers) [b2smb]"
     
     bl_options = {'REGISTER', 'UNDO'}
-    odd: bpy.props.BoolProperty(name = "Odd?", default=True)
-    pix_checker_size: bpy.props.IntProperty(name = "Checker Size", default=64)
-
+    odd: bpy.props.BoolProperty(name = "Odd # of Checkers", default=True)
+    pix_checker_size: bpy.props.IntProperty(name = "2x2 Checker Size", default=128, min=1)
 
     def execute(self, context):
         bpy.ops.uv.follow_active_quads()
         
-        #find checker size in uv units
+        # Find checker size in uv units
         checker_size = 1.0/(bpy.context.active_object.active_material.node_tree.nodes.get("Image Texture").image.size[0]/(self.pix_checker_size))
         
-        #first calculate all necessary vertex data
+        # First calculate all necessary vertex data
         active_obj = bpy.context.active_object        
         bm = bmesh.from_edit_mesh(active_obj.data)
         bm.verts.ensure_lookup_table() # Required after adding / removing vertices and before accessing them by index.
@@ -67,8 +66,6 @@ class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
         selected_loops = []
         active_loops = []
         
-
-        
         for f in selected_faces:
             if f != active_face:
                 for l in f.loops:
@@ -78,8 +75,7 @@ class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
         for l in active_face.loops:
             active_loops.append(l)
             
-
-        #determine direction
+        # Determine direction
         active_edges = [e for e in active_face.edges if len(e.link_faces) == 2]
         neighbor_faces = []
         for e in active_edges:
@@ -96,12 +92,12 @@ class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
         excl_verts = list(set(adj_verts) - set(intersect_verts))
         
         direction = (intersect_verts[0] - intersect_verts[1]).normalized().orthogonal()
-        #does it face the right way or must it be multiplied by -1?
+        # Does it face the right way or must it be multiplied by -1?
         approx_direction = (excl_verts[0] - intersect_verts[0]).normalized()
         if direction.angle(approx_direction) > math.pi/2.0:
             direction = -direction
             
-        #find the scaling factor
+        # Find the scaling factor
         origin = intersect_verts[0]
         selected_verts = list(set(l[uv_layer].uv.copy().freeze() for l in selected_loops))
         max_distance = 0.0
@@ -123,7 +119,7 @@ class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
         else:
            scaling_factor = even_scaling
         
-        #scale the uv coordinates by the scaling factor in the correct direction
+        # Scale the uv coordinates by the scaling factor in the correct direction
         
         for l in selected_loops:
             uv_vert = l[uv_layer].uv
@@ -135,13 +131,11 @@ class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
             uv_vert = uv_vert + origin
             l[uv_layer].uv = uv_vert
         
-        
         return {"FINISHED"}
-        
         
 class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
     bl_idname = "uv.smart_faq"
-    bl_label = "SMB Smart FAQ [b2smb]"
+    bl_label = "Follow Active Quads (Edges) [b2smb]"
     
     bl_options = {'REGISTER', 'UNDO'}
     include_active: bpy.props.BoolProperty(name = "Include Active Face?", default=True)
@@ -152,7 +146,7 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
     def execute(self, context):
         bpy.ops.uv.follow_active_quads()
         
-        #first calculate all necessary vertex data
+        # First calculate all necessary vertex data
         active_obj = bpy.context.active_object        
         bm = bmesh.from_edit_mesh(active_obj.data)
         bm.verts.ensure_lookup_table() # Required after adding / removing vertices and before accessing them by index.
@@ -164,8 +158,6 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
         selected_loops = []
         active_loops = []
         
-
-        
         for f in selected_faces:
             if f != active_face:
                 for l in f.loops:
@@ -175,8 +167,7 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
         for l in active_face.loops:
             active_loops.append(l)
             
-
-        #determine direction
+        # Determine direction
         active_edges = [e for e in active_face.edges if len(e.link_faces) == 2]
         neighbor_faces = []
         for e in active_edges:
@@ -196,12 +187,12 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
             direction = mathutils.Vector((1.0,0.0)).freeze()
         else:
             direction = mathutils.Vector((0.0,1.0)).freeze()
-        #does it face the right way or must it be multiplied by -1?
+        # Does it face the right way or must it be multiplied by -1?
         approx_direction = (excl_verts[0] - intersect_verts[0]).normalized()
         if direction.angle(approx_direction) > math.pi/2.0:
             direction = -direction
             
-        #find the scaling factor
+        # Find the scaling factor
         if self.include_active:
             origin = excl_verts[0]
         else:
@@ -215,8 +206,7 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
         
         scaling_factor = 0.5 * self.tile_number / max_distance
         
-        #scale the uv coordinates by the scaling factor in the correct direction
-        
+        # Scale the uv coordinates by the scaling factor in the correct direction
         if self.include_active:
             for l in active_face.loops:
                 selected_loops.append(l)
@@ -231,15 +221,12 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
             uv_vert = uv_vert + origin
             l[uv_layer].uv = uv_vert
         
-        
         return {"FINISHED"}
 
 def menu_func(self, context):
     self.layout.operator(VIEW3D_OT_cube_project_smb_checker.bl_idname)
     self.layout.operator(VIEW3D_OT_checker_FAQ.bl_idname)
     self.layout.operator(VIEW3D_OT_smart_FAQ.bl_idname)
-
-
 
 def handle_register():
    #  bpy.utils.register_class(MyOperatorClass)
